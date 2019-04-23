37773f084a8734f83c8d21cea7e0ddf15bec9eba
Your Name <btrangcal@gmail.com>
2019-04-23 14:51:06 -0700

    added todo comments

--- routes/choicesRoute.js
+++ routes/choicesRoute.js
@@ -4,6 +4,12 @@ const resultSchema = require('../models/Result');
 const _ = require('lodash');
 const File = mongoose.model('files',fileSchema);            //create a Files table
 const Result = mongoose.model('results',resultSchema);
+
+//TODO:
+//routes for specific files
+
+
+
 module.exports=app=>{
 
     app.post('/api/submitChoices',async (req,res)=>{
@@ -19,90 +25,14 @@ module.exports=app=>{
                 if(completedScenarioIdx===-1){
                     FileFound.completedScenarios.push(scenario);
                     await FileFound.save();
-                    //tally up results
-
-                    const questionIds = answers.map(ans=> ({questionID:Object.keys(ans)[0]}));
-                    console.log('questionIds',questionIds);
-                    for(let qId of questionIds){
-                        for(let key in qId){
-                            const ResultFound = await Result.findOne({questionID:qId[key]});
-                            let ansIdx = answers.findIndex(ans=>Object.keys(ans)[0]===qId[key]);
-                            if(ansIdx===-1)
-                                continue;
-                            console.log(answers[ansIdx]);
-                            if(ResultFound){
-                                choiceIdx=ResultFound.choices.findIndex(choice=>choice.choiceValue===answers[ansIdx][qId[key]])
-                                if(choiceIdx===-1){
-                                    console.log('cannot found choice within result');
-                                    const choiceObj = _.assign({choiceValue:null,total:0},{choiceValue:answers[ansIdx][qId[key]],total:1});
-                                    ResultFound.choices.push(choiceObj);
-                                    await ResultFound.save();
-                                    
-                                }
-                                else{
-                                    ResultFound.choices[choiceIdx].total= ResultFound.choices[choiceIdx].total +1;
-                                    await ResultFound.save();
-                                }
-                            }
-                            else{           //create new result
-                                console.log('creating new result');
-                                const choiceObj = _.assign({choiceValue:null,total:0},{choiceValue:answers[ansIdx][qId[key]],total:1})
-                                const arrChoices = [choiceObj];
-                                const result = await new Result({
-                                                                questionID:qId[key],   
-                                                                _file:_file, 
-                                                                choices:arrChoices   
-                                                        });
-                                await result.save();
-                                
-                            }
-                        }
-                    }
+                    await updateResultsWithUserAnswers(answers,_file);
                     res.send(FileFound);
 
                 }
                 else{       //overwrite old scenario within file
                     FileFound.completedScenarios[completedScenarioIdx]=scenario;
                     await FileFound.save();
-
-                    //tally up results
-                    const questionIds = answers.map(ans=> ({questionID:Object.keys(ans)[0]}));
-                    console.log('questionIds',questionIds);
-
-                    for(let qId of questionIds){
-                        for(let key in qId){
-                            const ResultFound = await Result.findOne({questionID:qId[key]});
-                            let ansIdx = answers.findIndex(ans=>Object.keys(ans)[0]===qId[key]);
-                            if(ansIdx===-1)
-                                continue;
-                            console.log(answers[ansIdx]);
-                            if(ResultFound){
-                                choiceIdx=ResultFound.choices.findIndex(choice=>choice.choiceValue===answers[ansIdx][qId[key]])
-                                if(choiceIdx===-1){
-                                    console.log('cannot found choice within result');
-                                    const choiceObj = _.assign({choiceValue:null,total:0},{choiceValue:answers[ansIdx][qId[key]],total:1});
-                                    ResultFound.choices.push(choiceObj);
-                                    await ResultFound.save();
-                                    
-                                }
-                                else{
-                                    ResultFound.choices[choiceIdx].total= ResultFound.choices[choiceIdx].total +1;
-                                    await ResultFound.save();
-                                }
-                            }
-                            else{           //create new result
-                                const choiceObj = _.assign({choiceValue:null,total:0},{choiceValue:answers[ansIdx][qId[key]],total:1})
-                                const arrChoices = [choiceObj];
-                                const result = await new Result({
-                                                                questionID:qId[key],   
-                                                                _file:_file, 
-                                                                choices:arrChoices   
-                                                        });
-                                await result.save();
-                                
-                            }
-                        }
-                    }
+                    await updateResultsWithUserAnswers(answers,_file);
                     res.send(FileFound);
                 }
 
@@ -159,3 +89,43 @@ module.exports=app=>{
     });
 }
 
+//takes in user answers and _file id
+//updates global results of user choices
+//by either updating Result Documents or making new ones if Result documents do not exist yet.  
+const updateResultsWithUserAnswers = async (answers,_file)=>{
+    const questionIds = answers.map(ans=> ({questionID:Object.keys(ans)[0]}));
+
+                    for(let qId of questionIds){
+                        for(let key in qId){
+                            const ResultFound = await Result.findOne({questionID:qId[key]});
+                            let ansIdx = answers.findIndex(ans=>Object.keys(ans)[0]===qId[key]);
+                            if(ansIdx===-1)
+                                continue;
+                            if(ResultFound){
+                                choiceIdx=ResultFound.choices.findIndex(choice=>choice.choiceValue===answers[ansIdx][qId[key]])
+                                if(choiceIdx===-1){ //no such choice ever submitted, we will create a new choice schema and push it into Result
+                                    console.log('cannot found choice within result');
+                                    const choiceObj = _.assign({choiceValue:null,total:0},{choiceValue:answers[ansIdx][qId[key]],total:1});
+                                    ResultFound.choices.push(choiceObj);
+                                    await ResultFound.save();    
+                                }
+                                else{
+                                    ResultFound.choices[choiceIdx].total= ResultFound.choices[choiceIdx].total +1;
+                                    await ResultFound.save();
+                                }
+                            }
+                            else{           //create new result
+                                console.log('creating new result');
+                                const choiceObj = _.assign({choiceValue:null,total:0},{choiceValue:answers[ansIdx][qId[key]],total:1})
+                                const arrChoices = [choiceObj];
+                                const result = await new Result({
+                                                                questionID:qId[key],   
+                                                                _file:_file, 
+                                                                choices:arrChoices   
+                                                        });
+                                await result.save();
+                                
+                            }
+                        }
+                    }
+}
