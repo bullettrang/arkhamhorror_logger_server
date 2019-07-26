173762816e3e6296fe4ff4a258c4c865124c0d24
Brian <trangb@oregonstate.edu>
2019-07-25 21:37:56 -0700

    NOZ campaign img s3

--- arkhamhorrorlogger2/src/constants/CampaignImages.js
+++ arkhamhorrorlogger2/src/constants/CampaignImages.js
@@ -9,7 +9,7 @@ import PC_IMG from '../Assets/CAMPAIGN_COVER/PC_form_img.jpg';
 import FA_IMG from '../Assets/CAMPAIGN_COVER/FA_form_img.jpg';
 
 export const CAMPAIGN_IMAGES ={
-    "Night of the Zealot":NOZ_IMG,
+    "Night of the Zealot":"https://arkham-horror-logger.s3.us-east-2.amazonaws.com/static/media/NOZ_form_img.961e476d.png",
     "The Dunwich Legacy":"http://arkham-horror-logger.s3.us-east-2.amazonaws.com/static/media/DWL_form_img.6a87e304.jpg",
     "The Path to Carcosa":PC_IMG,
     "The Forgotten Age":FA_IMG
