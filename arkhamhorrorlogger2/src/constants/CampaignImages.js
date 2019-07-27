f619ed14edef352ed4db5cd7b4f7bfe63a6bba5c
Brian <trangb@oregonstate.edu>
2019-07-27 12:51:28 -0700

    scenario imgs

--- arkhamhorrorlogger2/src/constants/CampaignImages.js
+++ arkhamhorrorlogger2/src/constants/CampaignImages.js
@@ -3,13 +3,13 @@
 // import PC_IMG from '../Assets/CAMPAIGN_COVER/PC_form_img.jpg';
 // import FA_IMG from '../Assets/CAMPAIGN_COVER/FA_form_img.jpg';
 
-// import NOZ_IMG from '../Assets/CAMPAIGN_COVER/NOZ_form_img.png';
+import NOZ_IMG from '../Assets/CAMPAIGN_COVER/NOZ_form_img.png';
 //import DWL_IMG from 'http://arkham-horror-logger.s3.us-east-2.amazonaws.com/static/media/DWL_form_img.6a87e304.jpg';
 // import PC_IMG from '../Assets/CAMPAIGN_COVER/PC_form_img.jpg';
 // import FA_IMG from '../Assets/CAMPAIGN_COVER/FA_form_img.jpg';
 
 export const CAMPAIGN_IMAGES ={
-    "Night of the Zealot":"https://arkham-horror-logger.s3.us-east-2.amazonaws.com/static/media/NOZ_form_img.961e476d.png",
+    "Night of the Zealot":"http://arkham-horror-logger.s3.us-east-2.amazonaws.com/static/media/DWL_form_img.6a87e304.jpg",
     "The Dunwich Legacy":"http://arkham-horror-logger.s3.us-east-2.amazonaws.com/static/media/DWL_form_img.6a87e304.jpg",
     "The Path to Carcosa":"https://arkham-horror-logger.s3.us-east-2.amazonaws.com/static/media/PC_form_img.4c8e7a40.jpg",
     "The Forgotten Age":"https://arkham-horror-logger.s3.us-east-2.amazonaws.com/static/media/FA_form_img.3aff9d40.jpg"
--- arkhamhorrorlogger2/src/constants/ScenarioImages.js
+++ arkhamhorrorlogger2/src/constants/ScenarioImages.js
@@ -1,28 +1,41 @@
-//NOZ SCENARIOS
-import gathering from '../Assets/SCENARIO_IMAGES/NOZ/the_gathering_art.jpg';
-import midnightMasks from '../Assets/SCENARIO_IMAGES/NOZ/the_midnight_masks_art.jpg';
-import devourer from '../Assets/SCENARIO_IMAGES/NOZ/the_devourer_below_art.jpg';
-//DWL SCENARIOS
-import extra from '../Assets/SCENARIO_IMAGES/DWL/extracurricular_activities_art.jpg';
-import house from '../Assets/SCENARIO_IMAGES/DWL/the_house_always_wins.jpg';
-import essex from '../Assets/SCENARIO_IMAGES/DWL/essex_county_express.jpg';
-import museum from '../Assets/SCENARIO_IMAGES/DWL/the_miskatonic_museum_art.jpg';
-import blood from '../Assets/SCENARIO_IMAGES/DWL/blood_on_the_altar.png';
-import undimensioned from '../Assets/SCENARIO_IMAGES/DWL/undimensioned_and_unseen.jpg';
-import doom from '../Assets/SCENARIO_IMAGES/DWL/where_doom_awaits.jpg';
-import lost from '../Assets/SCENARIO_IMAGES/DWL/lost_in_time_and_space.jpg';
+// //NOZ SCENARIOS
+// import gathering from '../Assets/SCENARIO_IMAGES/NOZ/the_gathering_art.jpg';
+// import midnightMasks from '../Assets/SCENARIO_IMAGES/NOZ/the_midnight_masks_art.jpg';
+// import devourer from '../Assets/SCENARIO_IMAGES/NOZ/the_devourer_below_art.jpg';
+// //DWL SCENARIOS
+// import extra from '../Assets/SCENARIO_IMAGES/DWL/extracurricular_activities_art.jpg';
+// import house from '../Assets/SCENARIO_IMAGES/DWL/the_house_always_wins.jpg';
+// import essex from '../Assets/SCENARIO_IMAGES/DWL/essex_county_express.jpg';
+// import museum from '../Assets/SCENARIO_IMAGES/DWL/the_miskatonic_museum_art.jpg';
+// import blood from '../Assets/SCENARIO_IMAGES/DWL/blood_on_the_altar.png';
+// import undimensioned from '../Assets/SCENARIO_IMAGES/DWL/undimensioned_and_unseen.jpg';
+// import doom from '../Assets/SCENARIO_IMAGES/DWL/where_doom_awaits.jpg';
+// import lost from '../Assets/SCENARIO_IMAGES/DWL/lost_in_time_and_space.jpg';
+
+// export const SCENARIO_IMGS={
+//     "The Gathering":gathering,
+//     "The Midnight Masks":midnightMasks,
+//     "The Devourer Below":devourer,
+//     "The House Always Wins":house,
+//     "The Miskatonic Museum":museum,
+//     "The Essex County Express":essex,
+//     "Blood on the Altar":blood,
+//     "Extracurricular Activities":extra,
+//     "Undimensioned and Unseen":undimensioned,
+//     "Where Doom Awaits":doom,
+//     "Lost in Time and Space":lost
+// }
 
 export const SCENARIO_IMGS={
-    "The Gathering":gathering,
-    "The Midnight Masks":midnightMasks,
-    "The Devourer Below":devourer,
-    "The House Always Wins":house,
-    "The Miskatonic Museum":museum,
-    "The Essex County Express":essex,
-    "Blood on the Altar":blood,
-    "Extracurricular Activities":extra,
-    "Undimensioned and Unseen":undimensioned,
-    "Where Doom Awaits":doom,
-    "Lost in Time and Space":lost
+    "The Gathering":"https://arkham-horror-logger.s3.us-east-2.amazonaws.com/static/media/the_gathering_art.258be215.jpg",
+    "The Midnight Masks":"https://arkham-horror-logger.s3.us-east-2.amazonaws.com/static/media/the_midnight_masks_art.b9c2c286.jpg",
+    "The Devourer Below":"https://arkham-horror-logger.s3.us-east-2.amazonaws.com/static/media/the_devourer_below_art.9b07d94f.jpg",
+    "The House Always Wins":"https://arkham-horror-logger.s3.us-east-2.amazonaws.com/static/media/the_house_always_wins.0d48cfa2.jpg",
+    "The Miskatonic Museum":"https://arkham-horror-logger.s3.us-east-2.amazonaws.com/static/media/the_miskatonic_museum_art.1cceec87.jpg",
+    "The Essex County Express":"https://arkham-horror-logger.s3.us-east-2.amazonaws.com/static/media/essex_county_express.17019448.jpg",
+    "Blood on the Altar":"https://arkham-horror-logger.s3.us-east-2.amazonaws.com/static/media/blood_on_the_altar.43af1062.png",
+    "Extracurricular Activities":"https://arkham-horror-logger.s3.us-east-2.amazonaws.com/static/media/extracurricular_activities_art.6aae4491.jpg",
+    "Undimensioned and Unseen":"https://arkham-horror-logger.s3.us-east-2.amazonaws.com/static/media/undimensioned_and_unseen.6de771e4.jpg",
+    "Where Doom Awaits":"https://arkham-horror-logger.s3.us-east-2.amazonaws.com/static/media/where_doom_awaits.99446c35.jpg",
+    "Lost in Time and Space":"https://arkham-horror-logger.s3.us-east-2.amazonaws.com/static/media/lost_in_time_and_space.9b4a310d.jpg"
 }
-
