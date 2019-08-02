cbe449a25c0132ec4cc8624a1f57049a5eb8db2b
Brian <trangb@oregonstate.edu>
2019-08-01 19:21:31 -0700

    forgot redirectURI for prod keys

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,6 @@ module.exports={
     googleClientID:process.env.GOOGLE_CLIENT_ID,
     googleClientSecret:process.env.GOOGLE_CLIENT_SECRET,
     mongoURI:process.env.MONGO_URI,
-    cookieKey:process.env.COOKIE_KEY       //i literally randomly typed this out, could be anything
+    cookieKey:process.env.COOKIE_KEY,       //i literally randomly typed this out, could be anything
+    redirectDomain:process.env.REDIRECT_URI
 }
